fe5585ceec480ad58a4150513e74ec13d5050421
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2023-04-13 16:45:36 -0300

    Add files via upload

---
+++ CASS-Alg-03-Ex-9.py
@@ -0,0 +1,29 @@
+dia = input("Digite um dia e um mês: ")
+
+janeiro = "01/01"
+abril = "21/04"
+maio = "01/05"
+setembro = "07/09"
+outubro = "12/10"
+novembro1 = "02/11"
+novembro2 = "15/11"
+dezembro = "25/12"
+
+if(dia == janeiro):
+    print("Confraternização universal")
+elif(dia == abril):
+    print("Tiradentes")
+elif(dia == maio):
+    print("Dia do trabalho")
+elif(dia == novembro1):
+    print("Finados")
+elif(dia == novembro1):
+    print("Finados")
+elif(dia == novembro1):
+    print("Finados")
+elif(dia == novembro2):
+    print("Proclamação da República")
+elif(dia == dezembro):
+    print("Natal")
+else:
+    print("Não é feriado")
