27ca8fc9ef8a2a446fab2c0ff564cd2a944ac501
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2023-04-13 14:47:34 -0300

    Add files via upload

--- CASS-Alg-03-Ex-5.py
+++ CASS-Alg-03-Ex-5.py
@@ -4,5 +4,7 @@ if(mes == "fevereiro"):
     print("28 ou 29 dias")
 elif(mes == "abril" or mes == "junho" or mes == "setembro" or mes == "novembro"):
     print("30 dias")
-else: 
+elif(mes == "janeiro" or mes == "março" or mes == "maio" or mes == "julho" or mes == "agosto" or mes == "outubro" or mes == "dezembro"): 
     print("31 dias")
+else:
+    print("Esse não é um mês do ano")
