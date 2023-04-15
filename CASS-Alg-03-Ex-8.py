a5116d67617e2b4b94341bb70fdd0a4f878e0045
Cecília <103593379+silvaceciliaa@users.noreply.github.com>
2023-04-14 23:14:17 -0300

    Update CASS-Alg-03-Ex-8.py

--- CASS-Alg-03-Ex-8.py
+++ CASS-Alg-03-Ex-8.py
@@ -1,26 +1,42 @@
-nota = input("Digite uma nota: ")
-oitava = int(input("Digite uma oitava: "))
+n = input("Digite uma nota: ")
+
+nota = n[0:1]
+oitava = int(n[1::])
 
 if(nota == "C" or nota == "c"):
+    
     frequencia = 261.63/(2**(4-oitava))
     print(frequencia)
+    
 elif(nota == "D" or nota == "d"):
+    
     frequencia = 293.66/(2**(4-oitava))
     print(frequencia)
+    
 elif(nota == "E" or nota == "e"):
+    
     frequencia = 329.63/(2**(4-oitava))
     print(frequencia)
+    
 elif(nota == "F" or nota == "f"):
+    
     frequencia = 349.23/(2**(4-oitava))
     print(frequencia)
+    
 elif(nota == "G" or nota == "g"):
+    
     frequencia = 392.00/(2**(4-oitava))
     print(frequencia)
+    
 elif(nota == "A" or nota == "a"):
+    
     frequencia = 440.00/(2**(4-oitava))
     print(frequencia)
+    
 elif(nota == "B" or nota == "b"):
+    
     frequencia = 493.88/(2**(4-oitava))
     print(frequencia)
+    
 else:
     print("Nota não encontrada")
